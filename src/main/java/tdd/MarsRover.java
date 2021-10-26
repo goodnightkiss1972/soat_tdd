ec1223319b4a028f567da3eb8bafc7f7bc717522
omarhammami1992 <omar.hammami.o.h@gmail.com>
2021-10-26 20:13:24 +0200

    first test of constructor

--- src/main/java/tdd/MarsRover.java
+++ src/main/java/tdd/MarsRover.java
@@ -1,4 +1,16 @@
 package tdd;
 
 public class MarsRover {
+
+    public MarsRover(Integer x, Integer y) {
+
+    }
+
+    public Integer getX() {
+        return 0;
+    }
+
+    public Integer getY() {
+        return 0;
+    }
 }
--- src/test/java/tdd/MarsRoverUTest.java
+++ src/test/java/tdd/MarsRoverUTest.java
@@ -1,7 +1,23 @@
 package tdd;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MarsRoverUTest {
 
+
+    @Test
+    void constructor_bind_position() {
+        // given
+        Integer x = 0;
+        Integer y = 0;
+
+        // when
+        final MarsRover marsRover = new MarsRover(x, y);
+
+        // then
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+    }
 }
